954b3bfcc2d62f48ab1436eed73fc626964ba0da
Ganessh Kumar R P <garp@microsoft.com>
2024-10-05 05:28:34 -0700

    adding newsletter subscriptioon

--- pages/index.tsx
+++ pages/index.tsx
@@ -170,6 +170,17 @@ const Home = (props) => {
         </div>
       </div>
 
+      <div className='bg-transparent mt-32'>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col rounded rounded-2xl bg-gradient-to-r from-violet-600 to-fuchsia-800'>
+          <div className='flex flex-col justify-center items-center my-12 '>
+            <div className='text-center px-8 text-2xl font-bold text-white'>Subscribe to our newsletter<br/>to get weekly updates about new plugins and plugin updates</div>
+          </div>
+          <div className='flex justify-center mb-12'>
+            <iframe src="https://obsidianpluginstats.substack.com/embed" width="480" height="320" style={{background: "#FF0000"}} frameborder="0" scrolling="no"></iframe>
+          </div>
+        </div>
+      </div>
+
       {/* Most Downloaded */}
       <div className='bg-transparent mt-32'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
