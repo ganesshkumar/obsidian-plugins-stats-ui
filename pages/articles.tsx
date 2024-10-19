f0ee46ee8d321414130eb2e8e7a52c64c6ee5afd
Ganessh Kumar R P <garp@microsoft.com>
2024-10-19 12:22:07 -0700

    removing articles

--- pages/articles.tsx
+++
@@ -1,7 +0,0 @@
-const Articles = () => {
-  return (
-    <div>
-      <div className="substack-post-embed"><p lang="en">Obsidian Plugin Updates 2024-09-28 to 2024-10-05 by Ganessh Kumar R P</p><p>We have five new plugins and eighty plugin updates in the last 7 days!</p><a data-post-link href="https://obsidianpluginstats.substack.com/p/obsidian-plugin-updates-2024-09-28">Read on Substack</a></div><script async src="https://substack.com/embedjs/embed.js" charset="utf-8"></script>
-    </div>
-  );
-}
